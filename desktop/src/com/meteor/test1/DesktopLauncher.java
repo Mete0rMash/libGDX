7515fa567b9c1e7c92efa7e25c57417cf7c1c9d6
Mete0rMash <ariel.a.sfeir@gmail.com>
2023-04-27 12:17:35 -0300

    Initial commit

---
+++ desktop/src/com/meteor/test1/DesktopLauncher.java
@@ -0,0 +1,17 @@
+package com.meteor.test1;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.meteor.test1.Test;
+
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Drop");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
+		new Lwjgl3Application(new Test(), config);
+	}
+}
---
+++ html/src/com/meteor/test1/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.meteor.test1.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.meteor.test1.Test;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Test();
+        }
+}
